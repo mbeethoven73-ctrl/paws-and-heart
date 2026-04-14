bf675670c6acbc37740a6d70d4f148d0421839d8
mbeethoven73-ctrl <email@example.com>
2026-04-14 14:39:54 +0800

    TUne hash rotuer

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
